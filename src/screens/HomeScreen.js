638f6aa45e7295076694be480ceb0ff8d7fc219a
Daniel Vela <d.vela1@hotmail.com>
2024-02-04 18:43:37 -0600

    Finished preview of WelcomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,36 +1,46 @@
 import React from 'react';
-import { SafeAreaView,Text } from 'react-native';
+import { SafeAreaView,Text, FlatList, StyleSheet,View } from 'react-native';
 import Header from '../components/Header';
 import TopBanner from '../components/TopBanner';
 import Section from '../components/Section'; // Import the Section component
 import PatientPreview from '../components/PatientPreview'; // Import the PatientPreview component
-import IconWithText from '../components/IconWithText';
-import { MaterialIcons } from '@expo/vector-icons'; // Import the Icon component
 import { commonStyles } from '../theme/theme';
 
+// Test Data
 const testPatient = {
   name: "John Doe",
   gender: "Male",
   age: 30
 };
+const testData = [testPatient,testPatient,testPatient,testPatient,testPatient,testPatient,testPatient,testPatient,testPatient]
+const DATA = Array.from({ length: 100 }, (_, i) => `Item ${i + 1}`);
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+
+
 const HomeScreen = () => {
   return (
     <SafeAreaView style={commonStyles.safeArea}>
-      <Header
-        message="Welcome to the App!"
-      />
-      {/* Other components of your screen */}
+      <Header message="Welcome to the App!"/>
       <TopBanner title={"Create a new patient profile"} 
       buttonTitle="CREATE PATIENT" />
-      <Section 
-      title ="Section Title">
-        <PatientPreview patient={testPatient}/>
-
-        
-      </Section>
-
-    </SafeAreaView>
+        <Section title ="Section Title">
+          <FlatList
+          data={testData}
+          renderItem={({ item }) => <PatientPreview patient={item} />}
+          keyExtractor={(item, index) => index.toString()}/>   
+          
+        </Section>
+      </SafeAreaView>
   );
+
 };
 
 export default HomeScreen;
+
+
